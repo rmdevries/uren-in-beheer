05934e8267b9b4fe450054fcd829136f12df1b78
Robert de Vries <robert.devries@coolblue.nl>
2020-10-06 18:44:25 +0200

    Create Unit Test for WorkShiftService for creation of WorkShift

---
+++ tests/UiB.Unit.Tests/WorkShift/WorkShiftServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentAssertions;
+using UiB.Application;
+using UiB.Domain.WorkShift;
+using Xunit;
+
+namespace UiB.Unit.Tests.WorkShift
+{
+    public class WorkShiftServiceTests
+    {
+        [Fact]
+        public void GivenValidWorkShift_WhenCreate_ThenReturnWorkShift()
+        {
+            DateTime start = DateTime.Now;
+            DateTime end = start.AddHours(1);
+            Domain.WorkShift.WorkShift workShift = new Domain.WorkShift.WorkShift(start, end);
+            IWorkShiftService workShiftService = new WorkShiftService();
+
+            var result = workShiftService.Create(workShift);
+
+            result.Start.Should().Be(start);
+            result.End.Should().Be(end);
+            result.Total.Should().Be(end.Subtract(start));
+        }
+    }
+}
