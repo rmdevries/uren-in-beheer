4167dcd857a0e7a5249cda1b1eac6463336bb78b
Robert de Vries <42142937+rmdevries@users.noreply.github.com>
2020-10-20 19:49:25 +0200

    Add, setup and customize SwaggerUI (#8)
    * Adding Swagger and updating some package versions
    
    * Setup SwaggerUI to the root
    
    * Customize the SwaggerUI with custom coloring

--- src/UiB.Api/Startup.cs
+++ src/UiB.Api/Startup.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace UiB.API
 {
@@ -16,25 +13,36 @@ namespace UiB.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "UiB API",
+                Description = "A small Time Registration System API"
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "UiB API V1");
+                c.RoutePrefix = string.Empty;
+                c.InjectStylesheet("/swagger-ui/custom.css");
+            });
+
+            app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
                 });
-            });
         }
     }
 }
